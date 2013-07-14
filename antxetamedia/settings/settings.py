385af2f93ea0ee8d36f29dd56a736a81b58456b2
Unai Zalakain <unai@gisa-elkartea.org>
2013-07-14 12:53:14 +0200

    add staticfiles to installed apps

--- antxetamedia/settings/settings.py
+++ antxetamedia/settings/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
+    'django.contrib.staticfiles',
     'grappelli.dashboard',
     'grappelli',
     'django.contrib.admin',
