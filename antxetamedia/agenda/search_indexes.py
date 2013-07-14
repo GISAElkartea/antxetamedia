0f8c6a1c1f361a83f44fda14420f4e4cba0cf431
Unai Zalakain <unai@gisa-elkartea.org>
2013-07-14 14:16:16 +0200

    typo

--- antxetamedia/agenda/search_indexes.py
+++ antxetamedia/agenda/search_indexes.py
@@ -4,14 +4,14 @@ from antxetamedia.agenda.models import Happening
 
 
 class HappeningIndex(indexes.SearchIndex, indexes.Indexable):
-    text = indexable.CharField(document=True, use_template=True)
+    text = indexes.CharField(document=True, use_template=True)
 
-    name = indexable.CharField(model_attr='name')
-    organizer = indexable.CharField(model_attr='organizer')
-    description = indexable.CharField(model_attr='description')
-    date = indexable.DateField(model_attr='date')
-    town = indexable.CharField()
-    place = indexable.CharField(model_attr='place')
+    name = indexes.CharField(model_attr='name')
+    organizer = indexes.CharField(model_attr='organizer')
+    description = indexes.CharField(model_attr='description')
+    date = indexes.DateField(model_attr='date')
+    town = indexes.CharField()
+    place = indexes.CharField(model_attr='place')
 
     def prepare_town(self, obj):
         if obj.town:
