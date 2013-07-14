b5085c0ddc9dbdb9f6d9291b3442c60e691b9c5b
Unai Zalakain <unai@gisa-elkartea.org>
2013-07-14 13:25:26 +0200

    add logging settings

--- antxetamedia/settings/settings.py
+++ antxetamedia/settings/settings.py
@@ -66,6 +66,25 @@ INSTALLED_APPS = (
     'antxetamedia.misc',
 )
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
+
+
 #PROGRAMMING
 PROGRAMMING_SPACE = 30
 
