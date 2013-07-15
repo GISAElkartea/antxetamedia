e7ef392738ad6ddce9a5a70b4b9f04086aee1050
Unai Zalakain <unai@gisa-elkartea.org>
2013-07-15 10:54:25 +0200

    add project migrations

---
+++ antxetamedia/projects/migrations/0001_initial.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Project'
+        db.create_table(u'projects_project', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=200)),
+            ('image', self.gf('django.db.models.fields.files.ImageField')(max_length=100, blank=True)),
+            ('text', self.gf('markitup.fields.MarkupField')(no_rendered_field=True)),
+            ('slug', self.gf('autoslug.fields.AutoSlugField')(unique=True, max_length=50, populate_from='name', unique_with=())),
+            ('_text_rendered', self.gf('django.db.models.fields.TextField')(blank=True)),
+        ))
+        db.send_create_signal(u'projects', ['Project'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'Project'
+        db.delete_table(u'projects_project')
+
+
+    models = {
+        u'projects.project': {
+            'Meta': {'object_name': 'Project'},
+            '_text_rendered': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'slug': ('autoslug.fields.AutoSlugField', [], {'unique': 'True', 'max_length': '50', 'populate_from': "'name'", 'unique_with': '()'}),
+            'text': ('markitup.fields.MarkupField', [], {'no_rendered_field': 'True'})
+        }
+    }
+
+    complete_apps = ['projects']
