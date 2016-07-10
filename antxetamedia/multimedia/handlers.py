d8247d43c8026a8de39b09856a3f7beb235dc4f6
Unai Zalakain <unai@gisa-elkartea.org>
2016-07-10 20:40:19 +0200

    Handle the case where the bucket already exists

--- antxetamedia/multimedia/handlers.py
+++ antxetamedia/multimedia/handlers.py
@@ -1,7 +1,9 @@
 from boto.s3.connection import S3Connection
-from boto.exception import S3ResponseError
+from boto.s3.bucket import Bucket
+from boto.exception import S3ResponseError, S3CreateError
 
 from django.conf import settings
+
 
 def upload(user, passwd, bucket, metadata, key, fd):
     conn = S3Connection(user, passwd, host=settings.S3_HOST, is_secure=False)
@@ -15,6 +17,9 @@ def upload(user, passwd, bucket, metadata, key, fd):
             bucket = conn.create_bucket(bucket, headers=metadata)
         except (S3ResponseError, UnicodeDecodeError):
             bucket = conn.create_bucket(bucket)
+        except S3CreateError as e:
+            if e.status == 409:
+                bucket = Bucket(conn, bucket)
 
     key = bucket.new_key(key)
     try:
