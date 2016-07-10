9bae155c284b6c9f3330abed0af7cd01d31a314c
Unai Zalakain <unai@gisa-elkartea.org>
2016-07-10 20:45:05 +0200

    Prepend the account name to the bucket name

--- antxetamedia/multimedia/models.py
+++ antxetamedia/multimedia/models.py
@@ -123,6 +123,7 @@ class Media(models.Model):
         else:
             data['bucket'] = 'antxeta'
             data['metadata']['x-archive-meta-title'] = 'Antxeta'
+        data['bucket'] = '{}-{}'.format(self.account.user.lower(), data['bucket'])
         data['metadata'] = dict(((k, 'uri({})'.format(v)) for k, v in data['metadata'].items()))
         data['key'] = '{0}-{1}{2}'.format(
                 data['bucket'], self.pk, splitext(self.local.name)[1])
