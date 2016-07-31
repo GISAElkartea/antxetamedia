da5269713a444c8a506535cd88f21fea8f1ffc83
Unai Zalakain <unai@gisa-elkartea.org>
2016-07-31 09:27:29 +0200

    Prepend S3 account username to buckets

--- antxetamedia/multimedia/handlers.py
+++ antxetamedia/multimedia/handlers.py
@@ -1,3 +1,5 @@
+from __future__ import unicode_literals
+
 from boto.s3.connection import S3Connection
 from boto.s3.bucket import Bucket
 from boto.exception import S3CreateError
@@ -8,7 +10,7 @@ from django.conf import settings
 def upload(user, passwd, bucket, metadata, key, fd):
     conn = S3Connection(user, passwd, host=settings.S3_HOST, is_secure=False)
 
-    bucket.strip('-')
+    bucket = '{}-{}'.format(user, bucket.strip('-'))
     try:
         bucket = conn.create_bucket(bucket, headers=metadata)
     except S3CreateError as e:
